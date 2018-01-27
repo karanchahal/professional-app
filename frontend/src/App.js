677bc4fc84353aa16faeefc523198217da22996c
karanchahal <karanbleep@gmail.com>
2018-01-27 16:52:49 +0530

    Fixed test import issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export class App extends Component {
+class App extends Component {
 
   render() {
     return (
@@ -18,5 +18,7 @@ export class App extends Component {
     );
   }
 }
+
+export default App;
 
 
